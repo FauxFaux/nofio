098ed64ceb9390e579c321c5a49dbd0865b0ce79
Chris West (Faux) <git@goeswhere.com>
2019-06-30 21:08:41 +0100

    closer

--- src/lib.rs
+++ src/lib.rs
@@ -2,9 +2,11 @@ use std::collections::{HashMap, VecDeque};
 use std::io;
 use std::io::Read;
 use std::io::Write;
+use std::mem;
 use std::net::SocketAddr;
 
 use failure::Error;
+use log::debug;
 use log::info;
 use mio::net::TcpListener;
 use mio::net::TcpStream;
@@ -42,13 +44,17 @@ struct Conn {
     inner: TcpStream,
     read_buffer: Stream,
     write_buffer: Stream,
-    wants_shutdown: bool,
 }
 
 struct Stream {
-    inner: Vec<u8>,
-    wanted: usize,
-    seen_eof: bool,
+    state: StreamState,
+}
+
+enum StreamState {
+    Normal { buf: Vec<u8>, wanted: usize },
+    Draining { buf: Vec<u8> },
+    AwaitingConfirmation,
+    Done,
 }
 
 enum Command {}
@@ -84,54 +90,153 @@ impl Default for CommandChannel {
 }
 
 impl Stream {
-    fn has_space(&self) -> bool {
-        self.inner.len() < self.wanted
+    fn read_interest(&self) -> bool {
+        match &self.state {
+            StreamState::Normal { buf, wanted } => buf.len() < *wanted,
+            StreamState::AwaitingConfirmation => true,
+            StreamState::Draining { .. } | StreamState::Done => false,
+        }
     }
 
-    fn is_empty(&self) -> bool {
-        self.inner.is_empty()
+    fn do_read(&self) -> bool {
+        match self.state {
+            StreamState::Normal { .. } | StreamState::AwaitingConfirmation => true,
+            StreamState::Draining { .. } | StreamState::Done => false,
+        }
+    }
+
+    fn write_interest(&self) -> bool {
+        match &self.state {
+            StreamState::Normal { buf, .. } | StreamState::Draining { buf } => !buf.is_empty(),
+            StreamState::AwaitingConfirmation => true,
+            StreamState::Done => false,
+        }
+    }
+
+    fn do_write(&self) -> bool {
+        match &self.state {
+            StreamState::Normal { buf, .. } | StreamState::Draining { buf } => !buf.is_empty(),
+            StreamState::AwaitingConfirmation => true,
+            StreamState::Done => false,
+        }
+    }
+
+    fn is_done(&self) -> bool {
+        match self.state {
+            StreamState::Done => true,
+            _ => false,
+        }
+    }
+
+    fn buf(&self) -> Option<&[u8]> {
+        match &self.state {
+            StreamState::Normal { buf, .. } | StreamState::Draining { buf } => Some(buf),
+            StreamState::AwaitingConfirmation | StreamState::Done => None,
+        }
+    }
+
+    fn buf_mut(&mut self) -> Option<&mut Vec<u8>> {
+        match &mut self.state {
+            StreamState::Normal { buf, .. } | StreamState::Draining { buf } => Some(buf),
+            StreamState::AwaitingConfirmation | StreamState::Done => None,
+        }
+    }
+
+    fn become_truncating_close(&mut self) {
+        debug!("become-truncating-close");
+        self.state = match self.state {
+            StreamState::Normal { .. } | StreamState::Draining { .. } => {
+                StreamState::AwaitingConfirmation
+            }
+            StreamState::AwaitingConfirmation | StreamState::Done => panic!("invalid state"),
+        };
+    }
+
+    fn become_draining_close(&mut self) {
+        debug!("become-draining-close");
+        let borrow_checker_avoid = StreamState::AwaitingConfirmation;
+        let mut temp_state = mem::replace(&mut self.state, borrow_checker_avoid);
+        self.state = match temp_state {
+            StreamState::Normal { buf, .. } => StreamState::Draining { buf },
+            StreamState::Draining { .. }
+            | StreamState::AwaitingConfirmation
+            | StreamState::Done => panic!("invalid state"),
+        };
+    }
+
+    fn totes_done(&mut self) {
+        debug!("totes-done");
+        self.state = StreamState::Done;
     }
 }
 
 impl Default for Stream {
     fn default() -> Stream {
         Stream {
-            inner: Vec::new(),
-            wanted: 8 * 1024,
-            seen_eof: false,
+            state: StreamState::Normal {
+                buf: Vec::new(),
+                wanted: 8 * 1024,
+            },
         }
     }
 }
 
 impl<'n> Io<'n> {
-    pub fn buf(&self) -> &[u8] {
-        match self.inner.tokens.get(&self.token).unwrap().mode {
-            OwnedMode::Conn(ref conn) => &conn.read_buffer.inner,
-            _ => unimplemented!("read on others"),
+    fn as_conn(&self) -> &Conn {
+        match self
+            .inner
+            .tokens
+            .get(&self.token)
+            .expect("io for non-conn")
+            .mode
+        {
+            OwnedMode::Conn(ref conn) => conn,
+            _ => unreachable!("must be a conn"),
         }
+    }
+
+    fn as_conn_mut(&mut self) -> &mut Conn {
+        match self
+            .inner
+            .tokens
+            .get_mut(&self.token)
+            .expect("io for non-conn")
+            .mode
+        {
+            OwnedMode::Conn(ref mut conn) => conn,
+            _ => unreachable!("must be a conn"),
+        }
+    }
+
+    pub fn buf(&self) -> &[u8] {
+        self.as_conn()
+            .read_buffer
+            .buf()
+            .expect("TODO: read buffer closed")
     }
 
     pub fn consume(&mut self, len: usize) {
-        match self.inner.tokens.get_mut(&self.token).unwrap().mode {
-            OwnedMode::Conn(ref mut conn) => drop(conn.read_buffer.inner.drain(..len)),
-            _ => unimplemented!("consume on others"),
-        }
+        drop(
+            self.as_conn_mut()
+                .read_buffer
+                .buf_mut()
+                .expect("TODO: read buffer closed")
+                .drain(..len),
+        )
     }
 
     pub fn write(&mut self, data: &[u8]) {
-        match self.inner.tokens.get_mut(&self.token).unwrap().mode {
-            OwnedMode::Conn(ref mut conn) => conn.write_buffer.inner.extend_from_slice(data),
-            _ => unimplemented!("write on others"),
-        }
+        self.as_conn_mut()
+            .write_buffer
+            .buf_mut()
+            .expect("TODO: write buffer closed")
+            .extend_from_slice(data)
     }
 
     pub fn close(&mut self) -> () {
-        match self.inner.tokens.get_mut(&self.token).unwrap().mode {
-            OwnedMode::Conn(ref mut conn) => {
-                conn.wants_shutdown = true;
-            }
-            _ => unimplemented!("close on others"),
-        }
+        let conn = self.as_conn_mut();
+        conn.read_buffer.become_truncating_close();
+        conn.write_buffer.become_draining_close();
     }
 }
 
@@ -194,23 +299,9 @@ impl Net {
             match &owned.mode {
                 OwnedMode::Server(_) => continue,
                 OwnedMode::Conn(conn) => {
-                    // we've completed reading, and all the data has been consumed,
-                    // and we've got nothing to write, or we can't write anymore
-                    let nothing_more_to_write = conn.wants_shutdown && conn.write_buffer.is_empty();
-                    if (conn.read_buffer.seen_eof && conn.read_buffer.is_empty())
-                        && (nothing_more_to_write || conn.write_buffer.seen_eof)
-                    {
+                    if conn.read_buffer.is_done() && conn.write_buffer.is_done() {
                         info!("{} closing", token.0);
                         to_close.push(*token);
-                    } else {
-                        info!(
-                            "{} not-closing: {} && {} && ({} || {})",
-                            token.0,
-                            conn.read_buffer.seen_eof,
-                            conn.read_buffer.is_empty(),
-                            nothing_more_to_write,
-                            conn.write_buffer.seen_eof
-                        );
                     }
                 }
             }
@@ -230,11 +321,11 @@ impl Net {
                 OwnedMode::Conn(conn) => {
                     let mut interest = Ready::empty();
 
-                    if conn.read_buffer.has_space() {
+                    if conn.read_buffer.read_interest() {
                         interest |= Ready::readable();
                     }
 
-                    if !conn.write_buffer.is_empty() {
+                    if !conn.write_buffer.write_interest() {
                         interest |= Ready::writable();
                     }
 
@@ -290,48 +381,67 @@ impl Net {
                                 inner: sock,
                                 read_buffer: Stream::default(),
                                 write_buffer: Stream::default(),
-                                wants_shutdown: false,
                             }),
                         },
                     );
                 }
                 OwnedMode::Conn(ref mut conn) => {
                     let mut buf = [0u8; BUF_SIZE];
-                    loop {
-                        if conn.read_buffer.seen_eof {
-                            break;
-                        }
-
-                        match block_to_none(conn.inner.read(&mut buf))? {
-                            Some(0) => {
-                                conn.read_buffer.seen_eof = true;
+                    while conn.read_buffer.do_read() {
+                        match conn.inner.read(&mut buf) {
+                            Ok(0) => {
+                                conn.read_buffer.become_draining_close();
                                 woke.push(Event::Done(ev.token(), Direction::Read));
                                 break;
                             }
 
-                            Some(r) => {
-                                conn.read_buffer.inner.extend_from_slice(&buf[..r]);
+                            Ok(r) => {
+                                conn.read_buffer
+                                    .buf_mut()
+                                    .expect("TODO: read completed on non-buffer")
+                                    .extend_from_slice(&buf[..r]);
                                 woke.push(Event::Data(ev.token()));
                             }
 
-                            None => break,
+                            Err(ref e) if io::ErrorKind::WouldBlock == e.kind() => break,
+
+                            Err(e) => {
+                                info!("{} read-err {:?}", ev.token().0, e);
+                                conn.read_buffer.become_truncating_close();
+                            }
                         }
                     }
 
-                    while !conn.write_buffer.seen_eof && !conn.write_buffer.inner.is_empty() {
-                        let w = match block_to_none(conn.inner.write(&conn.write_buffer.inner))? {
-                            Some(0) => {
-                                conn.write_buffer.seen_eof = true;
+                    while conn.write_buffer.do_write() {
+                        match conn.inner.write(
+                            conn.write_buffer
+                                .buf()
+                                .expect("asked to write, should be able to see data to write"),
+                        ) {
+                            Ok(0) => {
+                                info!("{} write-eof", ev.token().0);
+                                conn.write_buffer.totes_done();
                                 woke.push(Event::Done(ev.token(), Direction::Write));
                                 break;
                             }
-                            Some(w) => w,
-                            None => break,
-                        };
+                            Ok(w) => {
+                                drop(
+                                    conn.write_buffer
+                                        .buf_mut()
+                                        .expect("wrote data, should be able to discard it")
+                                        .drain(..w),
+                                );
+                                woke.push(Event::Data(ev.token()));
+                            }
 
-                        drop(conn.write_buffer.inner.drain(..w));
+                            Err(ref e) if io::ErrorKind::WouldBlock == e.kind() => break,
 
-                        woke.push(Event::Data(ev.token()));
+                            Err(e) => {
+                                info!("{} write-err {:?}", ev.token().0, e);
+                                conn.write_buffer.totes_done();
+                                break;
+                            }
+                        }
                     }
                 }
             }
